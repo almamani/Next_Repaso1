d406d3b9fdf13050960cf893989eedcc0e104bb9
almamani <al.mamani@gmail.com>
2025-06-04 11:28:35 -0300

    Configuraciones Iniciales

--- front/next.config.js
+++ front/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.macstation.com.ar",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "ocean-basic": "#129cb8",
+        "ocean-light": "#22d3ee",
+        "ocean-dark": "#0E7489",
+        "ocean-very-light": "#cffafe",
+        "black-basic": "#151515",
+        "black-light": "#374151",
+        "gray-basic": "#333333",
+        "gray-light": "#eeeeec",
+        "gray-very-light": "#f4f7fb",
+        "white-basic": "#ffffff",
+        "pink-basic": "#e62a65",
+        "pink-light": "#fad1de",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
